fc9f972e0262fbe4531d28ff67a08210275529a0
jsshin <tb25271@gmail.com>
2023-01-12 23:26:22 +0900

    [택배 배달 수거하기]

---
+++ 프로그래머스/KAKAO_TECH_INTERNSHIP_2022/택배배달과수거하기.java
@@ -0,0 +1,19 @@
+package 프로그래머스.KAKAO_TECH_INTERNSHIP_2022;
+
+public class 택배배달과수거하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(4,5,new int[]{1, 0, 3, 1, 2},new int[]{0, 3, 0, 4, 0});
+
+    }
+    public static long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = -1;
+        // 배달하기
+        int d_cap = cap;
+        for (int i = n-1; i >= 0; i--) {
+            if (cap > 0) {
+                deliveries[i] =
+            }
+        }
+        return answer;
+    }
+}
