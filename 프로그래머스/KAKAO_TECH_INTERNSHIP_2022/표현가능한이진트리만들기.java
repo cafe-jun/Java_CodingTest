21dbd761c224cfcf085eb2b8752fb7d3356169d2
jsshin <tb25271@gmail.com>
2023-01-18 22:49:25 +0900

    [표현가능한 이진트리]

---
+++ 프로그래머스/KAKAO_TECH_INTERNSHIP_2022/표현가능한이진트리만들기.java
@@ -0,0 +1,11 @@
+package 프로그래머스.KAKAO_TECH_INTERNSHIP_2022;
+
+public class 표현가능한이진트리만들기 {
+    public static void main(String[] args) {
+        System.out.println(new long[]{7, 42, 5});
+    }
+    public int[] solution(long[] numbers) {
+        int[] answer = {};
+        return answer;
+    }
+}
