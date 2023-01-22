9ea3a0bc9e2df0d3767095249b06d59d1d5dea75
jsshin <tb25271@gmail.com>
2023-01-22 23:00:09 +0900

    [중복순열]

--- 이론/순열.java
+++ 이론/순열.java
@@ -14,6 +14,18 @@ public class 순열 {
         }
     }
 
+    public static void duplicPermutation(int[] arr, int[] out, int depth, int r){
+        if(depth == r){
+            for(int num: out) System.out.print(num);
+            System.out.println();
+            return;
+        }
+        for(int i=0; i<arr.length; i++){
+            out[depth] = arr[i];
+            permutation(arr, out, depth+1, r);
+        }
+    }
+
     public static void main(String[] args){
         int[] arr = {1, 2, 3};
         int r = 4;
