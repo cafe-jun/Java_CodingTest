7b346d621bb789902dcfc620032f0d0d516ae1cf
jsshin <tb25271@gmail.com>
2023-01-30 23:34:39 +0900

    [

--- 프로그래머스/연습문제/무인도여행.java
+++ 프로그래머스/연습문제/무인도여행.java
@@ -13,9 +13,11 @@ public class 무인도여행 {
         int[] dr = {-1,0,1,0};
         int[] dc = {0,1,0,-1};
         int[] answer = {};
+        char[][] matrix = new char[maps.length][maps[0].length()];
         Queue<Point> pointList = new LinkedList();
         for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps.length; j++) {
+            for (int j = 0; j < maps[i].length(); j++) {
+                matrix[i][j] = maps[i].charAt(j);
                 if(maps[i].charAt(j) != 'X') {
                     pointList.offer(new Point(i,j));
                 }
@@ -25,18 +27,18 @@ public class 무인도여행 {
         // bfs
         while(!pointList.isEmpty()) {
             Point point = pointList.poll();
+            int score = Character.getNumericValue(matrix[point.r][point.c]);
             for (int i = 0; i < 4; i++) {
                 int nr = dr[i] + point.r;
                 int nc = dc[i] + point.c;
                 if (0 <= nr && nr < maps.length && 0 <= nc && nc < maps[0].length()) {
-                    if (maps[nr].charAt(nc) != 'X'){
-                       int temp = Character.getNumericValue(maps[point.r].charAt(point.c));
-
+                    if (matrix[nr][nc] != 'X'){
+                        matrix[nr][nc] = Character.forDigit(Character.getNumericValue(matrix[nr][nc])+score,10);
+                        pointList.offer(new Point(nr,nc));
                     }
                 }
             }
         }
-        System.out.println("무인도여행.solution");
 
         return answer;
     }
