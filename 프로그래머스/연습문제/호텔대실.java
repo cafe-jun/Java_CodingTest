2376dd4f39167a79b2e82463cec674c18c7e47a9
jsshin <tb25271@gmail.com>
2023-02-03 23:36:43 +0900

    [호텔대실] 오 한번에 풀음

--- 프로그래머스/연습문제/호텔대실.java
+++ 프로그래머스/연습문제/호텔대실.java
@@ -1,5 +1,6 @@
 package 프로그래머스.연습문제;
 
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class 호텔대실 {
@@ -13,23 +14,46 @@ public class 호텔대실 {
         int answer = 0;
         // book_time 다 초로 변경
         // 끝나는 시간이 있는지
-        PriorityQueue bookQueue = new PriorityQueue();
+        PriorityQueue<BookTime> bookQueue = new PriorityQueue();
+        for (int i = 0; i < book_time.length; i++) {
+            bookQueue.offer(new BookTime(book_time[i][0],book_time[i][1]));
+        }
+        ArrayList<BookTime> list = new ArrayList<>();
+        while(!bookQueue.isEmpty()) {
+            BookTime bookTime = bookQueue.poll();
+            // 만료 판단하기
+            for (int i = 0; i < list.size(); i++) {
+                if(bookTime.getStartTime() >= list.get(i).getEndTime()) {
+                    list.remove(i);
+                }
+            }
+            list.add(bookTime);
+            answer = Math.max(answer,list.size());
+        }
         return answer;
     }
 }
 
 class BookTime implements Comparable<BookTime>{
+    public int getEndTime() {
+        return endTime;
+    }
+    public int getStartTime() {
+        return startTime;
+    }
+
     private int startTime;
     private int endTime;
 
     public BookTime(String startTime, String endTime) {
-        startTime.split(':');
-        this.startTime = startTime.split(':');
-        this.endTime = endTime;
+        String[] splitStartTime = startTime.split(":");
+        this.startTime = Integer.parseInt(splitStartTime[0]) * 60 + Integer.parseInt(splitStartTime[1]);
+        String[] splitEndTime = endTime.split(":");
+        this.endTime = Integer.parseInt(splitEndTime[0])*60 + Integer.parseInt(splitEndTime[1])+10;
     }
 
     @Override
     public int compareTo(BookTime o) {
-        return 0;
+        return this.startTime<= o.startTime ? -1 : 1;
     }
 }
