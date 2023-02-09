99a0e50f90c56f70f77a16870fe768c0e3d8b506
jsshin <tb25271@gmail.com>
2023-02-09 23:11:13 +0900

    [유사칸토어비트열]

--- 프로그래머스/연습문제/유사칸토어비트열.java
+++ 프로그래머스/연습문제/유사칸토어비트열.java
@@ -6,30 +6,16 @@ public class 유사칸토어비트열 {
         System.out.println(solution(2,4,17));
     }
     public static int solution(int n, long l, long r) {
-        int answer = 0;
-        N = n;
-        String temp = recurisve(n);
-        for (long i = l; i < r; i++) {
-            if(temp == '1') {
-                answer += 1;
-            };
-        }
-        return answer;
-    }
-    public static String recurisve(int x) {
-        String str = "";
-        if(x==0) {
-            return "1";
-        }
-        str = recurisve(x-1);
-        String temp = "";
-        for (int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == '1') {
-                temp += "11011";
-            } else if (str.charAt(i) == '0') {
-                temp += "00000";
+        long answer = r-l+1;
+        for(long i=l-1;i<=r-1;i++){
+            for(int j=0;j<n;j++){
+                if((i/(int)Math.pow(5,j))%5==2){
+                    answer--;
+                    break;
+                }
             }
         }
-        return temp;
+
+        return (int) answer;
     }
 }
